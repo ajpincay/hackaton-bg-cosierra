a6924830008d6c943d1127571abb9dba61d7c513
Kevin Gonzalez <kevin-951206@hotmail.com>
2025-03-08 15:09:06 -0500

    Update create_tables.sql

--- db/create_tables.sql
+++ db/create_tables.sql
@@ -1,17 +1,18 @@
 DROP Table IF EXISTS pymes_trust;
 CREATE TABLE IF NOT EXISTS pymes_trust (
-                        id INT AUTO_INCREMENT PRIMARY KEY,  -- New column ID autoincremental as PK
-                        ruc VARCHAR(13) NOT NULL UNIQUE,
-                        pyme_name VARCHAR(35) NOT NULL,  -- This can be eliminated only for "academic purposes".
-                        trust_score SMALLINT NOT NULL
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    ruc VARCHAR(13) NOT NULL UNIQUE,
+    pyme_name VARCHAR(35) NOT NULL,
+    trust_score SMALLINT NOT NULL,
+    tier ENUM( 'N/A', 'Bronce', 'Plata', 'Oro') NOT NULL
 );
 
 DROP Table IF EXISTS mock_log_in;
 CREATE TABLE IF NOT EXISTS mock_log_in (
-                                     id INT AUTO_INCREMENT PRIMARY KEY,  -- New column ID autoincremental as PK
-                                     ruc VARCHAR(50) NOT NULL UNIQUE,
-                                     password VARCHAR(16) NOT NULL
-#                                      password_hash VARCHAR(255) NOT NULL  -- Enable this when hashing algorithm is implemented
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    ruc VARCHAR(50) NOT NULL UNIQUE,
+    password VARCHAR(16) NOT NULL
+#     password_hash VARCHAR(255) NOT NULL  -- Enable this when hashing algorithm is implemented
 );
 
 ALTER TABLE mock_log_in
