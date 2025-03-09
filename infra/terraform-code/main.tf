0bacce4eaa643821a5610183f1b0c6960b63e9d2
angel.pincay <ajpincay@live.com>
2025-03-09 06:31:47 -0500

    add load balancer

--- infra/terraform-code/main.tf
+++ infra/terraform-code/main.tf
@@ -101,6 +101,38 @@ resource "aws_vpc" "eks-vpc" {
   }
 }
 
+resource "aws_internet_gateway" "cosierra-igw" {
+  vpc_id = aws_vpc.eks-vpc.id
+  tags = {
+    Environment = var.env
+    Solution    = var.solutionName
+  }
+}
+
+resource "aws_route_table" "public" {
+  vpc_id = aws_vpc.eks-vpc.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.cosierra-igw.id
+  }
+
+  tags = {
+    Environment = var.env
+    Solution    = var.solutionName
+  }
+}
+
+resource "aws_route_table_association" "public-associantion" {
+  subnet_id      = aws_subnet.eks-subnet-a.id
+  route_table_id = aws_route_table.public.id
+}
+
+resource "aws_route_table_association" "public-associantion2" {
+  subnet_id      = aws_subnet.eks-subnet-b.id
+  route_table_id = aws_route_table.public.id
+}
+
 resource "aws_subnet" "eks-subnet-a" {
   vpc_id            = aws_vpc.eks-vpc.id
   cidr_block        = "10.0.1.0/24"
@@ -153,6 +185,106 @@ resource "aws_iam_role" "cosierra-eks-role" {
       Sid    = ""
     }]
   })
+}
+
+data "aws_eks_cluster" "k8s" {
+  name = aws_eks_cluster.cosierra-eks-cluster.name
+}
+
+data "aws_eks_cluster_auth" "k8s" {
+  name = aws_eks_cluster.cosierra-eks-cluster.name
+}
+
+resource "aws_lb" "cosierra-lb" {
+  name               = "cosierra-lb"
+  internal           = false
+  load_balancer_type = "application"
+  security_groups    = [aws_security_group.cosierra-sg.id]
+  subnets            = [aws_subnet.eks-subnet-a.id, aws_subnet.eks-subnet-b.id]
+
+  enable_deletion_protection = false
+
+  tags = {
+    Environment = var.env
+    Solution    = var.solutionName
+  }
+}
+
+resource "aws_security_group" "cosierra-sg" {
+  name        = "cosierra-lb-sg"
+  description = "Allow HTTP and HTTPS traffic"
+  vpc_id      = aws_vpc.eks-vpc.id
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+resource "aws_lb_listener" "http-listener" {
+  load_balancer_arn = aws_lb.cosierra-lb.arn
+  port              = 80
+  protocol          = "HTTP"
+
+  default_action {
+    type = "forward"
+
+    forward {
+      target_group {
+        arn = aws_lb_target_group.cosierra-tg.arn
+      }
+    }
+  }
+}
+
+#resource "aws_lb_listener" "https-listener" {
+#  load_balancer_arn = aws_lb.cosierra-lb.arn
+#  port              = 443
+#  protocol          = "HTTPS"
+
+#  ssl_policy = "ELBSecurityPolicy-2025"
+#  #certificate_arn = 
+
+#  default_action {
+#    type = "forward"
+
+#    forward {
+#      target_group {
+#        arn = aws_lb_target_group.cosierra-tg.arn
+#      }
+#    }
+#  }
+#}
+
+resource "aws_lb_target_group" "cosierra-tg" {
+  name     = "cosierra-tg"
+  port     = 80
+  protocol = "HTTP"
+  vpc_id   = aws_vpc.eks-vpc.id
+
+  health_check {
+    path                = "/"
+    interval            = "30"
+    timeout             = 5
+    healthy_threshold   = 2
+    unhealthy_threshold = 2
+  }
 }
 
 # Resource Group
