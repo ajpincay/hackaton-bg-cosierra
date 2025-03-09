f493f60e46ce88575da1c501e9ada7dd67e54420
Nmawyin <131683077+Nmawyin@users.noreply.github.com>
2025-03-09 11:15:30 -0500

    feat: add CreditOption and PymeCredit models, update bank routes to fetch credits from the database

--- backend/app/models/__init__.py
+++ backend/app/models/__init__.py
@@ -1,11 +1,14 @@
 # app/models/__init__.py
 from .base import Base
-from .pymes import PymeTrust, MockLogIn, PeerPymesTrustScore, PymeCertifications
+from .pymes import PymeTrust, MockLogIn, PeerPymesTrustScore, PymeCertifications, PymeConnection, CreditOption, PymeCredit
 
 __all__ = [
     "Base",
     "PymeTrust",
     "MockLogIn",
     "PeerPymesTrustScore",
-    "PymeCertifications"
+    "PymeCertifications",
+    "PymeConnection",
+    "CreditOption",
+    "PymeCredit",
 ]
--- backend/app/models/pymes.py
+++ backend/app/models/pymes.py
@@ -1,6 +1,6 @@
 # models/pymes.py
 import enum
-from sqlalchemy import Column, Integer, String, SmallInteger, Enum, Boolean, UniqueConstraint, DateTime, func
+from sqlalchemy import Column, Integer, String, SmallInteger, Enum, Boolean, UniqueConstraint, DateTime, func, Float
 from app.models.base import Base
 
 class TierEnum(enum.Enum):
@@ -59,4 +59,32 @@ class PymeConnection(Base):
     # Avoid duplicate requests
     __table_args__ = (
         UniqueConstraint("requester_ruc", "receiver_ruc", name="unique_connection"),
+    )
+
+class CreditOption(Base):
+    __tablename__ = "credit_options"
+    id = Column(Integer, primary_key=True, autoincrement=True)
+    title = Column(String(100), nullable=False)
+    description = Column(String, nullable=False)
+    amount = Column(String(50), nullable=False)
+    interest_rate = Column(String(20), nullable=False)
+    term = Column(String(50), nullable=False)
+    requirements = Column(String(100), nullable=False)
+    recommended = Column(Boolean, default=False)
+    link = Column(String(255), nullable=False)
+
+class PymeCredit(Base):
+    __tablename__ = "pyme_credits"
+    id = Column(Integer, primary_key=True, autoincrement=True)
+    ruc = Column(String(13), nullable=False)
+    credit_type_id = Column(Integer, nullable=False)
+    amount_approved = Column(Integer, nullable=False)
+    interest_rate = Column(Float, nullable=False)
+    term = Column(Integer, nullable=False)
+    start_date = Column(DateTime, nullable=False)
+    end_date = Column(DateTime, nullable=False)
+    progress_percentage = Column(SmallInteger, default=0)
+    status = Column(
+        Enum("Active", "Completed", "Cancelled", name="credit_status"),
+        default="Active"
     )
