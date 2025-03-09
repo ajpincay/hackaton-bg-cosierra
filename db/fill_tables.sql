184db1486f432103dbf5b2a9523c90e6baaf8fd8
Kevin Gonzalez <kevin-951206@hotmail.com>
2025-03-08 20:47:48 -0500

    Update fill_tables.sql

--- db/fill_tables.sql
+++ db/fill_tables.sql
@@ -1,3 +1,16 @@
+INSERT INTO pymes_trust (ruc, pyme_name) VALUES
+                                             ('1000223008', 'Yéssica_Cámara S.A.'),
+                                             ('1000336710', 'Caridad_Valle S.A.'),
+                                             ('1006137571', 'Eutimio_Carrera S.A.'),
+                                             ('1019283518', 'Esperanza_Adadia S.A.'),
+                                             ('1033690567', 'Abraham_Osuna S.A.'),
+                                             ('1045568214', 'Georgina_Georgina S.A.'),
+                                             ('1055059784', 'Filomena_Peralta S.A.'),
+                                             ('1181753730', 'Rodrigo_Asenjo S.A.'),
+                                             ('1420424791', 'Chelo_Peña S.A.'),
+                                             ('1733580873', 'Ramón_Gascón S.A.'),
+                                             ('1743559713', 'Maximiano_Jaén S.A.');
+
 INSERT INTO mock_log_in (ruc, password) VALUES
                                             ('1000223008', 'pssw123.'),
                                             ('1000336710', 'pssw123.'),
@@ -12,15 +25,4 @@ INSERT INTO mock_log_in (ruc, password) VALUES
                                             ('1743559713', 'pssw123.');
 
 
-INSERT INTO pymes_trust (ruc, pyme_name) VALUES
-                                            ('1000223008', 'Yéssica_Cámara S.A.'),
-                                            ('1000336710', 'Caridad_Valle S.A.'),
-                                            ('1006137571', 'Eutimio_Carrera S.A.'),
-                                            ('1019283518', 'Esperanza_Adadia S.A.'),
-                                            ('1033690567', 'Abraham_Osuna S.A.'),
-                                            ('1045568214', 'Georgina_Georgina S.A.'),
-                                            ('1055059784', 'Filomena_Peralta S.A.'),
-                                            ('1181753730', 'Rodrigo_Asenjo S.A.'),
-                                            ('1420424791', 'Chelo_Peña S.A.'),
-                                            ('1733580873', 'Ramón_Gascón S.A.'),
-                                            ('1743559713', 'Maximiano_Jaén S.A.');
+
