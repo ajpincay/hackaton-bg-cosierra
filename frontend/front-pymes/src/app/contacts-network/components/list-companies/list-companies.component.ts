abb5bee72e2a411704870477f3f8c291abe78d87
jordanfvc26 <jverac12@uteq.edu.ec>
2025-03-08 21:37:43 -0500

    Opciones del Dashboard y Red de contactos

--- frontend/front-pymes/src/app/contacts-network/components/list-companies/list-companies.component.ts
+++ frontend/front-pymes/src/app/contacts-network/components/list-companies/list-companies.component.ts
@@ -1,11 +1,22 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { MatTabsModule } from '@angular/material/tabs';
 
 @Component({
   selector: 'app-list-companies',
-  imports: [],
+  imports: [
+    CommonModule,
+    MatTabsModule
+  ],
   templateUrl: './list-companies.component.html',
   styleUrl: './list-companies.component.css'
 })
 export class ListCompaniesComponent {
+  //Variables
+  activeTab: string = 'todas';
 
+  changeTab(tab: string): void {
+    this.activeTab = tab;
+    console.log(this.activeTab);
+  }
 }
